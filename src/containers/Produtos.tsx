917f4547751026018b5c398efd9c9128cfa2b795
LucasDev <LucasDev9645@gmail.com>
2023-05-12 17:04:09 -0300

    Alterações para redux

--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,38 +1,26 @@
 import { Produto as ProdutoType } from '../App'
 import Produto from '../components/Produto'
-
+import { useGetProdutosQuery } from '../components/services/api'
 import * as S from './styles'
 
-type Props = {
-  produtos: ProdutoType[]
-  favoritos: ProdutoType[]
-  adicionarAoCarrinho: (produto: ProdutoType) => void
-  favoritar: (produto: ProdutoType) => void
-}
-
-const ProdutosComponent = ({
-  produtos,
-  favoritos,
-  adicionarAoCarrinho,
-  favoritar
-}: Props) => {
+const ProdutosComponent = () => {
   const produtoEstaNosFavoritos = (produto: ProdutoType) => {
     const produtoId = produto.id
-    const IdsDosFavoritos = favoritos.map((f) => f.id)
-
-    return IdsDosFavoritos.includes(produtoId)
+    const IdsDosFavoritos = produtos?.map((f) => f.id)
+    return IdsDosFavoritos?.includes(produtoId)
   }
+  const { data: produtos, isLoading } = useGetProdutosQuery()
+
+  if (isLoading) return <h2>Carregando Produtos...</h2>
 
   return (
     <>
       <S.Produtos>
-        {produtos.map((produto) => (
+        {produtos?.map((produto) => (
           <Produto
             estaNosFavoritos={produtoEstaNosFavoritos(produto)}
             key={produto.id}
             produto={produto}
-            favoritar={favoritar}
-            aoComprar={adicionarAoCarrinho}
           />
         ))}
       </S.Produtos>
---
+++ src/store/index.ts
@@ -0,0 +1,19 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
+
+import carrinhoReducer from './reducers/carrinho'
+import favoritoReducer from './reducers/favoritos'
+
+import api from '../components/services/api'
+
+export const store = configureStore({
+  reducer: {
+    carrinho: carrinhoReducer,
+    favorito: favoritoReducer,
+    [api.reducerPath]: api.reducer
+  },
+  middleware: (getDefaultMiddleware) => [
+    ...getDefaultMiddleware().concat(api.middleware)
+  ]
+})
+
+export type RootReducer = ReturnType<typeof store.getState>
